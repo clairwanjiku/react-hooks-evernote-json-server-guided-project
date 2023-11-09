c8fb061bca36490cecccd81bc9c58811ce59e615
Clair Wanjiku <clair.karanja@student.moringaschool.com>
2023-11-09 16:52:18 +0300

    completed assignment

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -1,7 +1,15 @@
-import React from 'react'
 
-export default function AnimalCard() {
+import React from 'react';
+
+const AnimalCard = ({ animal, handleLike, handleDetails }) => {
   return (
-    <div>AnimalCard</div>
-  )
-}
+    <div className="animal-card">
+      <img src={animal.image} alt={animal.name} />
+      <h3>{animal.name}</h3>
+      <button onClick={() => handleLike(animal.id)}>Like</button>
+      <button onClick={() => handleDetails(animal.id)}>Details</button>
+    </div>
+  );
+};
+
+export default AnimalCard;
