04e255b9e67a6e6661573cf1d5182c1fa72dc8f5
Clair Wanjiku <clair.karanja@student.moringaschool.com>
2023-11-09 16:57:30 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,3 +3,16 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './completed/Navbar';
 import DisplayAnimal from './completed/DisplayAnimal';
+
+const App = () => {
+  const [animals, setAnimals] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortedLetter, setSortedLetter] = useState('');
+  const [selectedAnimal, setSelectedAnimal] = useState(null);
+
+
+
+  return ()
+  <div>
+
+    </div>
