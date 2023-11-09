2536aa63fef4f622752aa17ba049faf62e0eb4eb
Clair Wanjiku <clair.karanja@student.moringaschool.com>
2023-11-09 16:52:53 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,1 @@
-import React from "react";
-import Header from "./Header";
-import 
-
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <NoteContainer />
-    </div>
-  );
-}
-
-export default App;
+import React, { useState, useEffect } from 'react';
