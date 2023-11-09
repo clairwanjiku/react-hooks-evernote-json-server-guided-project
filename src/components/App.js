b3e10dfc4394d30326302ae79eb4eb73dd6cd99b
Clair Wanjiku <clair.karanja@student.moringaschool.com>
2023-11-09 16:54:41 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Navbar from './completed/Navbar';
+import DisplayAnimal from './completed/DisplayAnimal';
